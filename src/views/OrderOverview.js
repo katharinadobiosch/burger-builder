803df0dca89317d44acf6931201f253dfd56f304
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2021-11-06 10:40:11 +0100

    added styling

--- src/views/OrderOverview.js
+++ src/views/OrderOverview.js
@@ -1,69 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { Link, useLocation } from "react-router-dom";
-
-// import OrderForm from "./OrderForm";
-// import OrderList from "./OrderList";
-// import Button from "../UI/Button";
-// import styles from "./OrderOverview.module.css";
-
-// const OrderOverview = (props) => {
-//     const [usersDataList, setUserList] = useState([]);
-//     const q = new URLSearchParams(useLocation().search);
-//     useEffect(() => {
-//         const userData = JSON.parse(q.get("payload"));
-//         setUserList(userData);
-//     }, []);
-//     const addOrderHandler = (
-//         userFirstName,
-//         userLastName,
-//         userPhone,
-//         userEmail,
-//         userStreet,
-//         userNumber,
-//         userCity,
-//         userZip,
-//         userComment
-//     ) => {
-//         setUserList((prevUsersList) => {
-//             return [
-//                 ...prevUsersList,
-//                 {
-//                     firstName: userFirstName,
-//                     lastName: userLastName,
-//                     phone: userPhone,
-//                     email: userEmail,
-//                     street: userStreet,
-//                     number: userNumber,
-//                     city: userCity,
-//                     zip: userZip,
-//                     comment: userComment,
-//                 },
-//             ];
-//         });
-//     };
-
-//     return (
-//         <>
-//             <div className={styles.flex_container}>
-//                 {/* <OrderForm onAddUserOrder={addOrderHandler} /> */}
-//                 <div>
-//                     <h1>Your order details</h1>
-//                 </div>
-//                 <div className={styles.flex_item}>
-//                     <OrderList users={usersDataList} />
-//                 </div>
-//                 <div className={styles.flex_item}>
-//                     <Link to="/ThankYou">
-//                         <Button>NEXT</Button>
-//                     </Link>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default OrderOverview;
-
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
@@ -73,59 +7,59 @@ import Button from "../UI/Button";
 import styles from "./OrderOverview.module.css";
 
 const OrderOverview = (props) => {
-    const [usersDataList, setUserList] = useState([]);
-    const q = new URLSearchParams(useLocation().search);
-    useEffect(() => {
-        const userData = JSON.parse(q.get("payload"));
-        setUserList(userData);
-    }, []);
-    const addOrderHandler = (
-        userFirstName,
-        userLastName,
-        userPhone,
-        userEmail,
-        userStreet,
-        userNumber,
-        userCity,
-        userZip,
-        userComment
-    ) => {
-        setUserList((prevUsersList) => {
-            return [
-                ...prevUsersList,
-                {
-                    firstName: userFirstName,
-                    lastName: userLastName,
-                    phone: userPhone,
-                    email: userEmail,
-                    street: userStreet,
-                    number: userNumber,
-                    city: userCity,
-                    zip: userZip,
-                    comment: userComment,
-                },
-            ];
-        });
-    };
+  const [usersDataList, setUserList] = useState([]);
+  const q = new URLSearchParams(useLocation().search);
+  useEffect(() => {
+    const userData = JSON.parse(q.get("payload"));
+    setUserList(userData);
+  }, []);
+  const addOrderHandler = (
+    userFirstName,
+    userLastName,
+    userPhone,
+    userEmail,
+    userStreet,
+    userNumber,
+    userCity,
+    userZip,
+    userComment
+  ) => {
+    setUserList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        {
+          firstName: userFirstName,
+          lastName: userLastName,
+          phone: userPhone,
+          email: userEmail,
+          street: userStreet,
+          number: userNumber,
+          city: userCity,
+          zip: userZip,
+          comment: userComment,
+        },
+      ];
+    });
+  };
 
-    return (
-        <>
-            <div className={styles.flex_container}>
-                {/* <OrderForm onAddUserOrder={addOrderHandler} /> */}
-                <div>
-                    <h1>Your order details</h1>
-                </div>
-                <div className={styles.flex_item}>
-                    <OrderList users={usersDataList} />
-                </div>
-                <div className={styles.flex_item}>
-                    <Link to="/ThankYou">
-                        <Button>NEXT</Button>
-                    </Link>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.flex_container}>
+        {/* <OrderForm onAddUserOrder={addOrderHandler} /> */}
+        <div>
+          <h1>Your order details</h1>
+        </div>
+        <div className={styles.flex_item}>
+          <OrderList users={usersDataList} />
+        </div>
+        <div className={styles.flex_item}>
+          <Link to="/ThankYou">
+            <Button>NEXT</Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default OrderOverview;
--- src/views/PlaceOrder.js
+++ src/views/PlaceOrder.js
@@ -1,84 +1,3 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import "../css/styles.css";
-// import styles from "./PlaceOrder.module.css";
-
-// import OrderForm from "./OrderForm";
-// import OrderList from "./OrderList";
-// import Button from "../UI/Button";
-
-// const PlaceOrder = (props) => {
-//     const [usersDataList, setUserList] = useState([]);
-//     // const [isVisible, setIsVisible] = useState(false);
-//     const [isVisible, setIsVisible] = useState(true);
-
-//     const addOrderHandler = (
-//         userFirstName,
-//         userLastName,
-//         userPhone,
-//         userEmail,
-//         userStreet,
-//         userNumber,
-//         userCity,
-//         userZip,
-//         userComment
-//     ) => {
-//         setUserList((prevUsersList) => {
-//             return [
-//                 ...prevUsersList,
-//                 {
-//                     firstName: userFirstName,
-//                     lastName: userLastName,
-//                     phone: userPhone,
-//                     email: userEmail,
-//                     street: userStreet,
-//                     number: userNumber,
-//                     city: userCity,
-//                     zip: userZip,
-//                     comment: userComment,
-//                 },
-//             ];
-//         });
-//     };
-
-//     const makeVisibleHandler = () => {
-//         setIsVisible(true);
-//     };
-
-//     return (
-//         <>
-//             <div className={styles.flex_container}>
-//                 <div className={styles.flex_item}>
-//                     <OrderForm onAddUserOrder={addOrderHandler} />
-//                 </div>
-
-//                 <div className={styles.flex_item}>
-//                     {!isVisible && (
-//                         <OrderList
-//                             users={usersDataList}
-//                             onAddingData={makeVisibleHandler}
-//                         />
-//                     )}
-//                 </div>
-
-//                 <div className={styles.flex_item}>
-//                     <Link
-//                         to={`/OrderOverview?payload=${JSON.stringify(
-//                             usersDataList
-//                         )}`}
-//                     >
-//                         <Button>NEXT 2</Button>
-//                     </Link>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default PlaceOrder;
-
-///////////////////////////////////////////////////////////
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../css/styles.css";
@@ -90,8 +9,7 @@ import Button from "../UI/Button";
 
 const PlaceOrder = (props) => {
     const [usersDataList, setUserList] = useState([]);
-    // const [isVisible, setIsVisible] = useState(false);
-    const [isVisible, setIsVisible] = useState(true);
+    const [isVisible, setIsVisible] = useState(false);
 
     const addOrderHandler = (
         userFirstName,
@@ -148,7 +66,7 @@ const PlaceOrder = (props) => {
                             usersDataList
                         )}`}
                     >
-                        <Button>NEXT 2</Button>
+                        <Button>NEXT</Button>
                     </Link>
                 </div>
             </div>
